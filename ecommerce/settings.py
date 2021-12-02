af67f4055fc3ca30976ed2571efe0ce00ff1e3c2
Melissa R <roulierm@users.noreply.github.com>
2021-12-01 20:36:21 -0500

    melissa

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
